4af48be90188c4f2a8cc3af98f057a7af40627cb
LamHuy <lamhuy5a5@gmail.com>
2022-11-17 21:16:35 +0700

    thunghiem

---
+++ Unitone/Tracnghiem2/TracnghiemDuolingo/Data/QuestionData.js
@@ -0,0 +1,159 @@
+const data =[
+    {
+        question:'1._______ to come over for dinner tonight?',
+        options: ['a. Do you want','b. Are you wanting ','c. Have you wanted ','d. Will you want'],
+        correct_option:'a. Do you want',
+        note:''
+    },
+    {
+        question:'2.When I _______ home last night, I _______ that Jane _______ a beautiful candlelight dinner.',
+        options: ['a. had arrived / discovered / prepared','b. was arriving / had discovered / was preparing','c. have arrived / was discovering / had prepared','d. arrived / discovered / was preparing'],
+        correct_option:'d. arrived / discovered / was preparing',
+        note:''
+    },
+    {
+        question:'3.    In the past the trip _______ very rough and often dangerous, but things _______ a great deal in the last hundred and fifty years.',
+        options: ['a. was / have changed ','b. is / change','c. had been / will change','d. has been / changed'],
+        correct_option:'a. was / have changed ',
+        note:''
+    },
+    {
+        question:'4.    In the last hundred years, traveling _______ much easier and more comfortable.',
+        options: ['a. becomes ','b. has become','c. became','d. will become'],
+        correct_option:'b. has become',
+        note:''
+    },
+    {
+        question:' -------------- in Rome than he --------------- . ',
+        options: ['a. No sooner he had arrived / was being kidnapped','b. No sooner had he arrived / was  kidnapped ','c. Had he no sooner  arrived / kidnapped','d. No sooner was he  arriving / had being kidnapped'],
+        correct_option:'b. No sooner had he arrived / was  kidnapped ',
+        note:''
+    },
+    {
+        question:"10.   I (A) didn’t see Frank  (B)since  (C) he and his wife  (D) moved to London . Haven’t seen",
+        options: ['A','B','C','D'],
+        correct_option:'A',
+        note:''
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default data
